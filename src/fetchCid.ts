83a1e8f4d9075e0b40bc768ee3daf07ae42f5bb8
futpib <futpib@gmail.com>
2025-12-01 17:55:47 +0000

    Use @futpib/fetch-cid package for IPFS fetching
    Replace custom FsCache and fetch implementation with the dedicated package
    that provides the same caching functionality.

--- src/fetchCid.ts
+++ src/fetchCid.ts
@@ -1,9 +1,5 @@
 import fsPromises from 'node:fs/promises';
-import path from 'node:path';
-import pMemoize from 'p-memoize';
-import envPaths from 'env-paths';
-
-const paths = envPaths('parser.futpib.github.io');
+import { fetchCid as fetchCidFromIpfs } from '@futpib/fetch-cid';
 
 function readableWebStreamOnFinish<T>(readableWebStream: ReadableStream<T>, onFinish: () => void): ReadableStream<T> {
 	const reader = readableWebStream.getReader();
@@ -34,74 +30,6 @@ function readableWebStreamOnFinish<T>(readableWebStream: ReadableStream<T>, onFi
 	return stream;
 }
 
-class FsCache {
-	private get _basePath() {
-		return path.join(paths.cache, 'fetchCid');
-	}
-
-	private _getKeyPath(key: string) {
-		return path.join(this._basePath, key.replaceAll('/', '_'));
-	}
-
-	async get(key: string): Promise<[ ReadableStream<Uint8Array>, ReadableStream<Uint8Array> ] | undefined> {
-		try {
-			const file = await fsPromises.open(this._getKeyPath(key), 'r');
-
-			const stream = file.readableWebStream() as ReadableStream<Uint8Array>;
-
-			const streamWithClose = readableWebStreamOnFinish(stream, () => {
-				file.close();
-			});
-
-			return [ streamWithClose, undefined as unknown as ReadableStream<Uint8Array> ];
-		} catch (error) {
-			if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
-				return undefined;
-			}
-
-			throw error;
-		}
-	}
-
-	async has(key: string) {
-		const streams = await this.get(key);
-		try {
-			return streams !== undefined;
-		} finally {
-			for (const stream of streams ?? []) {
-				await stream?.cancel();
-			}
-		}
-	}
-
-	async set(key: string, [ _, value ]: [ ReadableStream<Uint8Array>, ReadableStream<Uint8Array> ]) {
-		await fsPromises.mkdir(this._basePath, {
-			recursive: true,
-		});
-		const file = await fsPromises.open(this._getKeyPath(key), 'w');
-		try {
-			for await (const chunk of value) {
-				await file.write(chunk);
-			}
-		} finally {
-			await file.close();
-		}
-	}
-
-	async delete(key: string) {
-		await fsPromises.unlink(this._getKeyPath(key));
-	}
-}
-
-async function reallyFetchCid(cid: string): Promise<[ ReadableStream<Uint8Array>, ReadableStream<Uint8Array> ]> {
-	const response = await fetch('https://ipfs.io/ipfs/' + cid);
-	return response.body!.tee();
-}
-
-const cachedReallyFetchCid = pMemoize(reallyFetchCid, {
-	cache: new FsCache(),
-});
-
 export async function fetchCid(cidOrPath: string): Promise<AsyncIterable<Uint8Array>> {
 	if (cidOrPath.includes('/')) {
 		const file = await fsPromises.open(cidOrPath, 'r');
@@ -115,7 +43,5 @@ export async function fetchCid(cidOrPath: string): Promise<AsyncIterable<Uint8Ar
 		return streamWithClose;
 	}
 
-	const [ readable, unused ] = await cachedReallyFetchCid(cidOrPath);
-	await unused?.cancel();
-	return readable;
+	return fetchCidFromIpfs(cidOrPath);
 }
